1990bcde4045d785e1066232edae6026a863e716
Theo <theo.foncham@gmail.com>
2021-03-20 19:56:09 +0100

    added view html to render the model

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
+import { UserEdit } from './views/UserEdit';
 import { User } from './models/User';
-const collection = User.buildUserCollection();
 
-collection.on('change', () => {
-  console.log(collection);
-});
+const user = User.buildUser({ name: 'David', age: 32 });
 
-collection.fetch();
+const root = document.getElementById('root');
+
+if (root) {
+  const userEdit = new UserEdit(root, user);
+  userEdit.render();
+
+  console.log(userEdit);
+} else {
+  throw new Error('Root element not found');
+}
---
+++ src/views/UserForm.ts
@@ -0,0 +1,41 @@
+import { User, UserProps } from '../models/User';
+import { View } from './View';
+
+export class UserForm extends View<User, UserProps> {
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetNameClick,
+      'click:.save-model': this.onSaveClick,
+    };
+  }
+
+  onSaveClick = (): void => {
+    this.model.save();
+  };
+
+  onSetAgeClick = (): void => {
+    this.model.setRandomAge();
+  };
+
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector('input');
+
+    if (input) {
+      const name = input.value;
+      this.model.set({ name });
+    }
+  };
+
+  template(): string {
+    return `
+      <div>
+      <br>
+      <input placeholder="${this.model.get('name')}"/>
+      <button class="set-name">Change Name</button><br>
+      <button class="set-age">Set Random Age</button><br>
+      <button class="save-model">Save User</button><br>
+      </div>
+    `;
+  }
+}
